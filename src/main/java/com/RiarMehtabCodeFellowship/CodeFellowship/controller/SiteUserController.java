06b4624c4fc1f8fabeb4e8c7eb486d2ce4d37d8a
Mehtab Riar <mehtabriar228@hotmail.com>
2022-11-15 23:51:50 -0800

    finished lab

--- src/main/java/com/RiarMehtabCodeFellowship/CodeFellowship/controller/SiteUserController.java
+++ src/main/java/com/RiarMehtabCodeFellowship/CodeFellowship/controller/SiteUserController.java
@@ -1,18 +1,22 @@
 package com.RiarMehtabCodeFellowship.CodeFellowship.controller;
 
+import com.RiarMehtabCodeFellowship.CodeFellowship.models.Post;
 import com.RiarMehtabCodeFellowship.CodeFellowship.models.SiteUser;
+import com.RiarMehtabCodeFellowship.CodeFellowship.repository.PostRepository;
 import com.RiarMehtabCodeFellowship.CodeFellowship.repository.SiteUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.Date;
 
 @Controller
 public class SiteUserController {
@@ -26,17 +30,42 @@ public class SiteUserController {
     @Autowired
     HttpServletRequest request;
 
+    @Autowired
+    PostRepository postRepository;
+
     // Principal == Http session
     @GetMapping("/")
     public String getHomePage(Model m, Principal p){
         if(p != null){
             String username = p.getName();
             SiteUser foundUser = siteUserRepo.findByUsername(username);
-
+            m.addAttribute("posts", foundUser.getPostList());
             m.addAttribute("username", username);
-            m.addAttribute("fName", foundUser.getFirstName());
+            m.addAttribute("name", foundUser.getFirstName());
+            m.addAttribute("users", siteUserRepo.findAll());
         }
-        return "profile";
+        return "myprofile";
+    }
+
+    @PostMapping("/friend/{friendname}")
+    public String getUserInfo(Model m, Principal p, @PathVariable String friendname){
+        SiteUser siteUser = siteUserRepo.findByUsername(p.getName());
+        m.addAttribute("username", siteUser.getUsername());
+
+        SiteUser friend = siteUserRepo.findByUsername(friendname);
+        m.addAttribute("friendname", friend.getUsername());
+        m.addAttribute("posts", friend.getPostList());
+
+        return "friend";
+    }
+
+    @PostMapping("/addPost")
+    public RedirectView addPost(String body, String username, String title){
+        SiteUser siteuser = siteUserRepo.findByUsername(username);
+        Date date = new Date();
+        Post newPost = new Post(body, title);
+        postRepository.save(newPost);
+        return new RedirectView("/");
     }
 
     @GetMapping("/profile")
